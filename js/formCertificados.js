4fd05563f6e76b244ae7de24e4de6905210aef7e
agoscollado <youremail.com>
2025-06-19 09:00:36 -0300

    js (validacion)para certificados con patterns

---
+++ js/formCertificados.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const formulario = document.getElementById("formulario");
+
+        formulario.addEventListener("submit", function (e) {
+        if (!verificarDatos(e)) {
+            e.preventDefault();
+        }
+    });
+});
+
+function verificarDatos(e) {
+    let valido = true;
+    const campos = document.querySelectorAll("#formulario input, #formulario textarea");
+
+    campos.forEach(campo => {
+        campo.classList.remove("is-valid", "is-invalid");
+
+        if (!campo.checkValidity()) {
+            campo.classList.add("is-invalid");
+            valido = false;
+        } else {
+            campo.classList.add("is-valid");
+        }
+    });
+
+    return valido;
+}
